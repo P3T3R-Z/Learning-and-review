c43c989e2495cf41b0cb12e8c4e47ccfe4db002f
P3T3R_Z <1611216092@qq.com>
2019-04-08 13:38:26 +0800

    增加plugin基础用法, sourceMap,中间件容器

--- webpackStudy/webpack.config.js
+++ webpackStudy/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -6,12 +8,25 @@ module.exports = {
     //entry: './src/index.js',
     entry: {
         main: './src/index.js',
-        other: './src/index2.js'    //多个入口
+        other: './src/index2.js' //多个入口
+    },
+    devtool: 'inline-source-map', //sourceMap
+
+    devServer: {
+        contentBase: './dist'
     },
 
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({ //替换html内容插件
+            title: '123'
+        })
+    ],
+
     output: {
-        filename: '[name].js',   //占位符保证每个文件具有唯一名称
-        path: path.resolve(__dirname, 'dist')
+        filename: '[name].js', //占位符保证每个文件具有唯一名称
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'     //使得静态文件能够在localhost:3000下正确访问
     },
 
     //loader 运行其他类型文件
