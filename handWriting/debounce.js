3bc25c6e29926a0d410394bb2ae0f00ba570cd46
17798738997 <1611216092@qq.com>
2020-06-24 17:10:45 +0800

    更新节流防抖手写函数

--- handWriting/debounce.js
+++ handWriting/debounce.js
@@ -1,9 +1,21 @@
-function debounce(fn,time){
-    let timer=null;
-    if(timer){
-        clearTimeout(timer)
+//把触发非常频繁的事件合并成一次执行
+var debounce = function(action, delay) {
+    var timer = null;
+    
+    return function() {
+        var self = this, 
+              args = arguments;
+              
+        clearTimeout(timer);
+        timer = setTimeout(function() {
+            action.apply(self, args)
+        }, delay);
     }
-    timer = setTimeout(
-        fn(),time
-    )
 }
+
+// example
+function resizeHandler() {
+    console.log("resize");
+}
+
+window.onresize = debounce(resizeHandler, 300);
--- handWriting/throttle.js
+++ handWriting/throttle.js
@@ -1,8 +1,20 @@
-function throttle(fn,time){
-    let timer=0;
-    if(!timer){
-        timer = setTimeout(
-            fn.apply(this),time
-        )
+//设置一个阀值，在阀值内，把触发的事件合并成一次执行；当到达阀值，必定执行一次事件
+var throttleV2 = function(action, delay){
+    var statTime = 0;
+    
+    return function() {
+        var currTime = +new Date();
+        
+        if (currTime - statTime > delay) {
+            action.apply(this, arguments);
+            statTime = currTime ;
+        }
     }
+}    
+
+// example
+function resizeHandler() {
+    console.log("resize");
 }
+
+window.onresize = throttleV2(resizeHandler, 300);
