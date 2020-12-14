5b293af7f422c2e777881f722754e99eda009e8b
17798738997 <1611216092@qq.com>
2020-12-14 16:31:04 +0800

    vue3 ts

--- vue3/demo/src/main.ts
+++ vue3/demo/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import Axios from "axios";
-import baseMixin from "@/mixin/baseMixin"
+// import baseMixin from "@/mixin/baseMixin.ts"
 
 // createApp(App).mount('#app')
 
@@ -11,5 +11,5 @@ const app = createApp(App);
 //绑定到全局
 app.config.globalProperties.Axios=Axios
  
-app.mixin(baseMixin)
+// app.mixin(baseMixin)
 app.mount('#app')
