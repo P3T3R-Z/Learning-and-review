f9760ff5db6e86e859b5aef91ec794043f12eb04
17798738997 <1611216092@qq.com>
2021-01-28 17:32:48 +0800

    study

--- handWriting/instanceof.js
+++ handWriting/instanceof.js
@@ -8,3 +8,10 @@ function myInstance(left,right){
         proto = proto.__proto__;
     }
 }
+/*instanceof 运算符用于测试构造函数的 prototype 属性是否出现在对象原型链中的任何位置
+换句话说，如果A instanceof B，那么 A 必须是一个对象，而 B 必须是一个合法的 JavaScript 函数。在这两个条件都满足的情况下：
+判断 B 的 prototype 属性指向的原型对象(B.prototype)是否在对象 A 的原型链上。
+
+如果在，则为 true；如果不在，则为 false。
+instanceof 的实现原理，其实思路也很简单，无非就是一个沿原型链向上查找的过程
+*/
